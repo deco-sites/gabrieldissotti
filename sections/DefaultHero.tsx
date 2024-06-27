82b9dcb73f40fe70836ce7e1e8e1e1e0ae59e076
Gabriel Dissotti <gabriel.dissotti@stone.com.br>
2024-06-27 00:03:42 +0000

    Release portfolio-home environment with 3 changes made by Gabriel Dissotti

---
+++ sections/DefaultHero.tsx
@@ -0,0 +1,32 @@
+interface Props {
+  title?: string;
+  description?: string;
+}
+
+export default function Section({
+  title = "Olá, eu sou o Gabriel Dissotti",
+  description = "Desenvolvedor de sites",
+}: Props) {
+  return (
+    <div className="h-[calc(100dvh-32px)] flex flex-col justify-center items-center">
+      <div className="pb-20 flex flex-col md:flex-row-reverse md:items-center gap-4 md:gap-6 lg:gap-8">
+        <div>
+          <div className="flex flex-col gap-4 md:gap-6 lg:gap-8">
+            <h1 className="font-bold md:font-semibold text-4xl md:text-7xl lg:text-8xl leading-normal text-black">
+              {title}
+            </h1>
+
+            <p className="font-semibold text-xl md:text-2xl lg:text-3xl leading-normal text-zinc-400 min-h-[90px] sm:min-h-0">
+              {description}
+              aaa
+            </p>
+          </div>
+
+          <div className="mt-10 lg:mt-14 flex flex-col items-center gap-4 md:flex-row">
+            Botões
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
