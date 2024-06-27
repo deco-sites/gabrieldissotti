2a4cc2daca75142827fb65ce8840d987d44c4d82
Gabriel Dissotti <gabriel.dissotti@stone.com.br>
2024-06-27 01:48:49 +0000

    Release portfolio-home environment with 0 changes made by

--- sections/DefaultHero.tsx
+++ sections/DefaultHero.tsx
@@ -8,7 +8,7 @@ export default function Section({
   description = "Desenvolvedor de sites",
 }: Props) {
   return (
-    <div className="h-[calc(100dvh-32px)] flex flex-col justify-center items-center">
+    <div className="m-auto max-w-2xl h-[calc(100dvh-32px)] flex flex-col justify-center items-center">
       <div className="pb-20 flex flex-col md:flex-row-reverse md:items-center gap-4 md:gap-6 lg:gap-8">
         <div>
           <div className="flex flex-col gap-4 md:gap-6 lg:gap-8">
@@ -18,12 +18,11 @@ export default function Section({
 
             <p className="font-semibold text-xl md:text-2xl lg:text-3xl leading-normal text-zinc-400 min-h-[90px] sm:min-h-0">
               {description}
-              aaa
             </p>
           </div>
 
           <div className="mt-10 lg:mt-14 flex flex-col items-center gap-4 md:flex-row">
-            Botões
+            Botões 2 a
           </div>
         </div>
       </div>
